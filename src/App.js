87ac1e58c2634fd553fa43ceaec3d11409b6a5d5
risana <frisana225@gmail.com>
2023-12-15 09:56:25 -0500

    initial project

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import { useState } from 'react';
 
-const App = () => {
-  const [teams, setTeams] = useState([]);
-  const [inputType, setInputType] = useState("");
 
-  const addTeam = () => {
-    if (inputType.trim() !== "") {
-      setTeams([...teams, inputType.trim()]);
-      setInputType("");
-    }
-  };
-
-  const onChangeInput = (e) => {
-    setInputType(e.target.value);
-  };
-
+function App() {
   return (
     <div className="App">
-      <center>
-        <h2>Team Management App</h2>
-        <input
-          type="text"
-          placeholder="Enter team name"
-          onChange={onChangeInput}
-          value={inputType}
-        />
-        <button onClick={addTeam}>Add</button>
-        <ol>
-          {teams.map((team, index) => (
-            <li key={index}>{team}</li>
-          ))}
-        </ol>
-      </center>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,4 @@
+const Home = () => (
+    <div>helloworld</div>
+)
+export default Home;
