1aadfad063f43ab335954a4fe377ac16e68b5de4
midelaj <i.midelaj@gmail.com>
2023-12-30 19:04:27 +0530

    form

--- src/pages/ExpenseTracking/api.js
+++ src/pages/ExpenseTracking/api.js
@@ -1,16 +1,16 @@
-export const CreateExpense = async () => {
+import { toast } from "react-toastify";
+import { getToken } from "utils/storage";
+
+export const createExpense = async (expense) => {
   try {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_HOST}/expenses`,
-      {
-        body: JSON.stringify(),
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${getToken()}`,
-        },
-      }
-    );
+    const response = await fetch(`${process.env.REACT_APP_API_HOST}/expenses`, {
+      body: JSON.stringify(expense),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getToken()}`,
+      },
+    });
     const jsonResponse = await response.json();
     return jsonResponse;
   } catch (error) {
--- src/pages/ExpenseTracking/index.js
+++ src/pages/ExpenseTracking/index.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Formik, Form } from "formik";
-import { CreateExpense } from "./api"; // Assuming CreateExpense is the function to create an expense
-import * as Yup from "yup";
+import { Formik, Field, Form } from "formik";
+import * as yup from "yup";
 import {
   Container,
   Row,
   Col,
   Form as BootstrapForm,
   Button,
+  FormGroup,
+  FormLabel,
+  FormControl,
+  InputGroup,
 } from "react-bootstrap";
 
 // Validation schema using Yup
-const validationSchema = Yup.object().shape({
-  expenseType: Yup.string().required("Expense type is required"),
-  date: Yup.date().required("Date is required"),
-  amount: Yup.number().positive().required("Amount is required"),
-  remark: Yup.string(),
+const validationSchema = yup.object().shape({
+  expenseType: yup.string().required("Expense type is required"),
+  date: yup.date().required("Date is required"),
+  amount: yup.number().positive().required("Amount is required"),
+  remark: yup.string(),
 });
 
 // Expense types options
@@ -31,10 +34,8 @@ const ExpenseForm = () => {
   const handleSubmit = async (values, { setSubmitting, resetForm }) => {
     try {
       // Call the CreateExpense function here
-      await CreateExpense(values);
-
-      // Handle success, you can show an alert or perform other actions
-      console.log("Expense submitted successfully!");
+      // Replace with your actual function and logic
+      console.log("Submitting expense:", values);
     } catch (error) {
       // Handle errors, you can show an error alert or perform other actions
       console.error("Error submitting expense:", error);
@@ -46,32 +47,104 @@ const ExpenseForm = () => {
   };
 
   return (
-    <Container className="mt-5">
-      <Row className="justify-content-center">
-        <Col md={6}>
-          <Formik
-            initialValues={{
-              expenseType: "",
-              date: "",
-              amount: "",
-              remark: "",
-            }}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ isSubmitting }) => (
-              <Form as={BootstrapForm}>
-                {/* ... (existing form fields) */}
+    <Formik
+      initialValues={{
+        expenseType: "",
+        date: "",
+        amount: "",
+        remark: "",
+      }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        values,
+        touched,
+        errors,
+        isSubmitting,
+      }) => (
+        <BootstrapForm noValidate onSubmit={handleSubmit}>
+          <Row className="mb-3">
+            <FormGroup as={Col} md="4" controlId="validationFormik01">
+              <FormLabel>Expense Type</FormLabel>
+              <FormControl
+                as="select"
+                name="expenseType"
+                value={values.expenseType}
+                onChange={handleChange}
+                isInvalid={touched.expenseType && !!errors.expenseType}
+              >
+                <option value="" disabled>
+                  Select an option
+                </option>
+                {expenseTypeOptions.map((type) => (
+                  <option key={type} value={type}>
+                    {type}
+                  </option>
+                ))}
+              </FormControl>
+              <FormControl.Feedback type="invalid">
+                {errors.expenseType}
+              </FormControl.Feedback>
+            </FormGroup>
 
-                <Button variant="primary" type="submit" disabled={isSubmitting}>
-                  {isSubmitting ? "Submitting..." : "Save Expense"}
-                </Button>
-              </Form>
-            )}
-          </Formik>
-        </Col>
-      </Row>
-    </Container>
+            <FormGroup as={Col} md="4" controlId="validationFormik02">
+              <FormLabel>Date</FormLabel>
+              <FormControl
+                type="date"
+                name="date"
+                value={values.date}
+                onChange={handleChange}
+                isInvalid={touched.date && !!errors.date}
+              />
+              <FormControl.Feedback type="invalid">
+                {errors.date}
+              </FormControl.Feedback>
+            </FormGroup>
+
+            <FormGroup as={Col} md="4" controlId="validationFormikUsername">
+              <FormLabel>Amount</FormLabel>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">$</InputGroup.Text>
+                <FormControl
+                  type="number"
+                  placeholder="Amount"
+                  aria-describedby="inputGroupPrepend"
+                  name="amount"
+                  value={values.amount}
+                  onChange={handleChange}
+                  isInvalid={touched.amount && !!errors.amount}
+                />
+                <FormControl.Feedback type="invalid">
+                  {errors.amount}
+                </FormControl.Feedback>
+              </InputGroup>
+            </FormGroup>
+          </Row>
+
+          <FormGroup className="mb-3">
+            <FormLabel>Remark</FormLabel>
+            <FormControl
+              as="textarea"
+              rows={3}
+              name="remark"
+              value={values.remark}
+              onChange={handleChange}
+              isInvalid={touched.remark && !!errors.remark}
+            />
+            <FormControl.Feedback type="invalid">
+              {errors.remark}
+            </FormControl.Feedback>
+          </FormGroup>
+
+          <Button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Save Expense"}
+          </Button>
+        </BootstrapForm>
+      )}
+    </Formik>
   );
 };
 
